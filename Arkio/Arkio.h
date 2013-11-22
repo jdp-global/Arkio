8c06c075cdd807ea0d288b2a82933fe2b5dd3225
Ray Scott <ray@alienhitcher.com>
2013-11-22 10:17:42 +0000

    Initial Commit

---
+++ Arkio/Arkio.h
@@ -0,0 +1,13 @@
+//
+//  Arkio.h
+//  Arkio
+//
+//  Created by Ray Scott on 22/11/2013.
+//  Copyright (c) 2013 Alien Hitcher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Arkio : NSObject
+
+@end
---
+++ Arkio/Arkio.m
@@ -0,0 +1,13 @@
+//
+//  Arkio.m
+//  Arkio
+//
+//  Created by Ray Scott on 22/11/2013.
+//  Copyright (c) 2013 Alien Hitcher. All rights reserved.
+//
+
+#import "Arkio.h"
+
+@implementation Arkio
+
+@end
