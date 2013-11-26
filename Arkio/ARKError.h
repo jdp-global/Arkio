63703326bdf69d5339c5aa518c04384f9fef35ea
Ray Scott <ray@alienhitcher.com>
2013-11-26 13:14:20 +0000

    Represents an error in the Arkio library.

---
+++ Arkio/ARKError.h
@@ -0,0 +1,59 @@
+//
+//  ARKError.h
+//  Arkio
+//
+//  Created by Ray Scott on 25/11/2013.
+//  Copyright (c) 2013 Alien Hitcher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+/**
+ *  Represents an error in the Arkio library
+ */
+@interface ARKError : NSError
+
+#pragma mark - Designated Object Initializers
+
+/**
+ *  Creates an error with the default ARK error domain and the given parameters.
+ *
+ *  @param code    The error code for the error.
+ *  @param message The message for the error.
+ *
+ *  @return An NSError object in the `ARKErrorDomain` with the specified error code and message.
+ */
++ (id)errorWithCode:(NSInteger)code message:(NSString *)message;
+
+/**
+ *  Creates an error with the default ARK error domain and the given parameters.
+ *
+ *  @param code The error code for the error.
+ *  @param dict The userInfo dictionary for the error. userInfo may be nil.
+ *
+ *  @return An NSError object in the `ARKErrorDomain` with the specified error code and the dictionary of arbitrary data userInfo.
+ */
++ (id)errorWithCode:(NSInteger)code userInfo:(NSDictionary *)dict;
+
+/**
+ *  Creates an error with the default ARK error domain and the given parameters.
+ *
+ *  @param code The error code for the error.
+ *  @param dict The userInfo dictionary for the error. userInfo may be nil.
+ *
+ *  @return An NSError object in the `ARKErrorDomain` with the specified error code and the dictionary of arbitrary data userInfo.
+ */
+- (id)initWithCode:(NSInteger)code userInfo:(NSDictionary *)dict;
+
+@end
+
+/**
+ *  Arkio Error Domain
+ */
+extern NSString *const ARKErrorDomain;
+
+/**
+ *  Default message for unknown errors.
+ */
+extern NSString *const ARKAnUnknownErrorOccurredMessageKey;
---
+++ Arkio/ARKError.m
@@ -0,0 +1,41 @@
+//
+//  ARKError.m
+//  Arkio
+//
+//  Created by Ray Scott on 25/11/2013.
+//  Copyright (c) 2013 Alien Hitcher. All rights reserved.
+//
+
+#import "ARKError.h"
+
+// Error Domain
+NSString *const ARKErrorDomain = @"com.alienhitcher.arkio.ErrorDomain";
+
+// Error Message Keys (for localization)
+NSString *const ARKAnUnknownErrorOccurredMessageKey = @"An unknown error occurred.";
+
+@implementation ARKError
+
+#pragma mark - Designated Object Initializers
+
++ (id)errorWithCode:(NSInteger)code message:(NSString *)message
+{
+    NSDictionary *userInfo = [NSDictionary dictionaryWithObject:message forKey:NSLocalizedDescriptionKey];
+    return [NSError errorWithDomain:ARKErrorDomain code:code userInfo:userInfo];
+}
+
++ (id)errorWithCode:(NSInteger)code userInfo:(NSDictionary *)dict
+{
+    return [NSError errorWithDomain:ARKErrorDomain code:code userInfo:dict];
+}
+
+- (id)initWithCode:(NSInteger)code userInfo:(NSDictionary *)dict
+{
+    self = [super initWithDomain:ARKErrorDomain code:code userInfo:dict];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
