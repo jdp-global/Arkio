7316af763fc89798748da16f5817cffef759ce03
Ray Scott <ray@alienhitcher.com>
2013-11-26 13:13:15 +0000

    Shared string keys for API entities.

---
+++ Arkio/ARKEntity.h
@@ -0,0 +1,59 @@
+//
+//  ARKEntity.h
+//  Arkio
+//
+//  Created by Ray Scott on 26/11/2013.
+//  Copyright (c) 2013 Alien Hitcher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  Model Entity 
+ */
+@interface ARKEntity : NSObject
+
+@end
+
+// common keys
+extern NSString * kARKAddressKey;
+extern NSString * kARKCityKey;
+extern NSString * kARKStateKey;
+extern NSString * kARKCountryKey;
+extern NSString * kARKZipKey;
+extern NSString * kARKPhoneKey;
+extern NSString * kARKUpdatedDateKey;
+
+// contact keys
+extern NSString * kARKContactIDKey;
+extern NSString * kARKTitleKey;
+extern NSString * kARKCompanyNameKey;
+extern NSString * kARKFirstNameKey;
+extern NSString * kARKLastNameKey;
+extern NSString * kARKContactURLKey;
+extern NSString * kARKAreaCodeKey;
+extern NSString * kARKEmailKey;
+extern NSString * kARKOwnedKey;
+extern NSString * kARKOwnedTypeKey;
+
+// company keys
+extern NSString * kARKCompanyIDKey;
+extern NSString * kARKNameKey;
+extern NSString * kARKWebsiteKey;
+extern NSString * kARKStockSymbolKey;
+extern NSString * kARKStockExchangeKey;
+extern NSString * kARKOwnershipKey;
+extern NSString * kARKFortuneRankKey;
+extern NSString * kARKEmployeeCountKey;
+extern NSString * kARKEmployeeRangeKey;
+extern NSString * kARKRevenueRangeKey;
+extern NSString * kARKIndustry1Key;
+extern NSString * kARKIndustry2Key;
+extern NSString * kARKIndustry3Key;
+extern NSString * kARKSubIndustry1Key;
+extern NSString * kARKSubIndustry2Key;
+extern NSString * kARKSubIndustry3Key;
+extern NSString * kARKSICCodeKey;
+extern NSString * kARKActiveContactsKey;
+extern NSString * kARKLinkInJigsawKey;
+extern NSString * kARKPointsKey;
---
+++ Arkio/ARKEntity.m
@@ -0,0 +1,58 @@
+//
+//  ARKEntity.m
+//  Arkio
+//
+//  Created by Ray Scott on 26/11/2013.
+//  Copyright (c) 2013 Alien Hitcher. All rights reserved.
+//
+
+#import "ARKEntity.h"
+
+// Common keys
+NSString const * kARKAddressKey     = @"address";
+NSString const * kARKCityKey        = @"city";
+NSString const * kARKStateKey       = @"state";
+NSString const * kARKCountryKey     = @"country";
+NSString const * kARKZipKey         = @"zip";
+NSString const * kARKPhoneKey       = @"phone";
+NSString const * kARKUpdatedDateKey = @"udpatedDate";
+
+// Contact keys
+NSString const * kARKContactIDKey   = @"contactId";
+NSString const * kARKTitleKey       = @"title";
+NSString const * kARKCompanyNameKey = @"companyName";
+NSString const * kARKFirstNameKey   = @"firstName";
+NSString const * kARKLastNameKey    = @"lastName";
+NSString const * kARKContactURLKey  = @"contactURL";
+NSString const * kARKAreaCodeKey    = @"areaCode";
+NSString const * kARKEmailKey       = @"email";
+NSString const * kARKOwnedKey       = @"owned";
+NSString const * kARKOwnedTypeKey   = @"ownedType";
+
+// Company keys
+NSString const * kARKCompanyIDKey       = @"companyId";
+NSString const * kARKNameKey            = @"name";
+NSString const * kARKWebsiteKey         = @"website";
+NSString const * kARKStockSymbolKey     = @"stockSymbol";
+NSString const * kARKStockExchangeKey   = @"stockExchange";
+NSString const * kARKOwnershipKey       = @"ownership";
+NSString const * kARKFortuneRankKey     = @"fortuneRank";
+NSString const * kARKEmployeeCountKey   = @"employeeCount";
+NSString const * kARKEmployeeRangeKey   = @"employeeRange";
+NSString const * kARKRevenueRangeKey    = @"revenueRange";
+NSString const * kARKIndustry1Key       = @"industry1";
+NSString const * kARKIndustry2Key       = @"industry2";
+NSString const * kARKIndustry3Key       = @"industry3";
+NSString const * kARKSubIndustry1Key    = @"subIndustry1";
+NSString const * kARKSubIndustry2Key    = @"subIndustry2";
+NSString const * kARKSubIndustry3Key    = @"subIndustry3";
+NSString const * kARKSICCodeKey         = @"sicCode";
+NSString const * kARKActiveContactsKey  = @"activeContacts";
+NSString const * kARKLinkInJigsawKey    = @"linkInJigsaw";
+
+// User keys
+NSString const * kARKPointsKey       = @"points";
+
+@implementation ARKEntity
+
+@end
