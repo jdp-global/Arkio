9171efba4b6a6a06a4bb9570b54e2c62607d737a
Ray Scott <ray@alienhitcher.com>
2013-11-26 13:13:54 +0000

    NSError codes in ARKErrorDomain.

---
+++ Arkio/ARKErrors.h
@@ -0,0 +1,17 @@
+//
+//  ARKErrors.h
+//  Arkio
+//
+//  Created by Ray Scott on 26/11/2013.
+//  Copyright (c) 2013 Alien Hitcher. All rights reserved.
+//
+
+/* NSError codes in ARKErrorDomain.
+ */
+enum {
+    
+    ARKUnknownError = -1,
+    
+    ARKJSONSerializationError = 1       // error has occurred in underlying JSON Serialization call.
+};
+
