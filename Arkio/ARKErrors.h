bbc3398c1c12b0bebf9711fb968bc7ecd7f57507
Ray Scott <ray@alienhitcher.com>
2013-11-30 17:41:42 +0000

    Added Data.com API error codes.

--- Arkio/ARKErrors.h
+++ Arkio/ARKErrors.h
@@ -15,6 +15,19 @@ enum {
     ARKJSONSerializationError   = 1,        // error has occurred in underlying JSON Serialization call
     ARKInvalidArgumentsError    = 2,        // method contract wasn't upheld
     ARKValueNoFoundError        = 3,        // expected value is missing
-    ARKValidationError          = 4         // invalid data encountered
+    ARKValidationError          = 4,         // invalid data encountered
+    
+    
+    // Data.com API Error Codes
+    // 400
+    ARKDataDotComParameterError         = 400,  // bad request E.G missing or incorrect parameter
+    ARKDataDotComForbiddenError         = 403,  // typical failure, E.G. authentication failure, or incorrect developer token
+    ARKDataDotComNotFoundError          = 404,  // attempt to access non-existent data, E.G. contact not found for ID
+    ARKDataDotComMethodNotAllowedError  = 405,  // E.G. on contact not owned.
+
+    // 500
+    ARKDataDotComInternalServerError    = 500,  // typically caused by a system or search error
+    ARKDataDotComNotSupportedError      = 501,  // attempted to acceess an unimplemented feature
+    ARKDataDotComNotAvailableError      = 503   // the service is unavailable
 };
 
