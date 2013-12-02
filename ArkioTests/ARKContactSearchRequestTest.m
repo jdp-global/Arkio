d34833dcdf85d1352b33be20f5466cc5a141a320
Ray Scott <ray@alienhitcher.com>
2013-12-02 18:37:49 +0000

    Uses new test data dictionary. Improved testing and error messages.

--- ArkioTests/ARKContactSearchRequestTest.m
+++ ArkioTests/ARKContactSearchRequestTest.m
@@ -18,11 +18,29 @@
 
 - (void)testContactSearchRequest
 {
-    [self.session searchContactsWithString:@"Ray"
-                                    offset:0
-                                      size:20
+    // reference data
+    NSString *firstLast = (NSString *)[self testDataForKey:@"arkio.xctest.contact.firstlast"];
+    NSNumber *offset = (NSNumber *)[self testDataForKey:@"arkio.xctest.search.offset"];
+    NSNumber *size = (NSNumber *)[self testDataForKey:@"arkio.xctest.search.size"];
+    
+    [self.session searchContactsWithString:firstLast
+                                    offset:[offset intValue]
+                                      size:[size intValue]
                                    success:^(ARKContactSearchResult *result, ARKError *error) {
-                                       NSLog(@"blah");
+
+                                       XCTAssertNotNil(result, @"%s no result object returned", __PRETTY_FUNCTION__);
+                                       XCTAssertTrue([result totalHits] > 0, @"%s total hits returned is 0", __PRETTY_FUNCTION__);
+                                       
+                                       NSSet *contacts = [result contacts];
+                                       XCTAssertNotNil(contacts, @"%s contacts set is nil", __PRETTY_FUNCTION__);
+                                       XCTAssertTrue([contacts count] > 0, @"%s contacts set is empty", __PRETTY_FUNCTION__);
+
+                                       NSArray *names = [firstLast componentsSeparatedByString:@" "];
+                                       NSString *firstName = [names firstObject];
+                                       NSString *lastName = [names lastObject];
+                                       XCTAssertTrue([[[contacts anyObject] firstName] isEqualToString:firstName], @"%s incorrect first name in results for contact search", __PRETTY_FUNCTION__);
+                                       XCTAssertTrue([[[contacts anyObject] lastName] isEqualToString:lastName], @"%s incorrect last name in results for contact search", __PRETTY_FUNCTION__);
+                                       
                                        [self signalFinished];
                                    }
                                    failure:^(NSError *error) {
