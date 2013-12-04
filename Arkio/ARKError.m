af880dc0a44ac586b29eafb969a073386f7fac78
Ray Scott <ray@alienhitcher.com>
2013-12-04 19:52:32 +0000

    Added errorWithCode:httpStatusCode:message:stackTrace: initializer. Added DDC error code string constants.

--- Arkio/ARKError.m
+++ Arkio/ARKError.m
@@ -14,9 +14,43 @@ NSString * const ARKErrorDomain = @"com.alienhitcher.arkio.ErrorDomain";
 // Error Message Keys (for localization)
 NSString * const ARKAnUnknownErrorOccurredMessageKey = @"An unknown error occurred.";
 
+// Additional userInfo dictionary keys
+NSString * const ARKErrorAPIErrorCodeKey = @"ARKErrorAPIErrorCodeKey";
+NSString * const ARKErrorAPIStackTraceKey = @"ARKErrorAPIStackTraceKey";
+
+// DDC Error Codes
+NSString * const ARKDDCParameterErrorCode = @"PARAM_ERROR";
+NSString * const ARKDDCLoginFailureErrorCode = @"LOGIN_FAIL";
+NSString * const ARKDDCTokenFailureErrorCode = @"TOKEN_FAIL";
+NSString * const ARKDDCPurchaseLowPointsErrorCode = @"PURCHASE_LOW_POINTS";
+NSString * const ARKDDCContactNotFoundErrorCode = @"CONTACT_NOT_EXIS";
+NSString * const ARKDDCContactNotOwnedErrorCode = @"CONTACT_NOT_OWNED";
+NSString * const ARKDDCSearchErrorCode = @"SEARCH_ERROR";
+NSString * const ARKDDCSystemErrorCode = @"SYS_ERROR";
+NSString * const ARKDDCNotImplementedErrorCode = @"NOT_IMPLEMENTED";
+NSString * const ARKDDCNotAvailableErrorCode = @"NOT_AVAILABLE";
+
+
 @implementation ARKError
 
 #pragma mark - Designated Object Initializers
+
++ (instancetype)errorWithCode:(NSString *)code
+               httpStatusCode:(NSInteger)httpStatusCode
+                      message:(NSString *)message
+                   stackTrace:(NSString *)stackTrace
+{
+    if (!message) {
+        message = ARKAnUnknownErrorOccurredMessageKey;
+    }
+    
+    NSMutableDictionary *userInfo = [NSMutableDictionary dictionary];
+    userInfo[ARKErrorAPIErrorCodeKey] = code;
+    userInfo[NSLocalizedDescriptionKey] = message;
+    userInfo[ARKErrorAPIStackTraceKey] = stackTrace;
+    
+    return [[ARKError alloc] initWithCode:httpStatusCode userInfo:userInfo];
+}
 
 + (id)errorWithCode:(NSInteger)code message:(NSString *)message
 {
